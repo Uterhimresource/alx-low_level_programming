b300fa07524568dcbc1b5a18c15ee1f6d390e893
Uterhimresource <uterhimresource@gmail.com>
2023-02-17 17:24:49 -0800

    print combination number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function where excution begin
+ * Description: display base 10 numbers from 0 t0 9
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n < 9)
+		{
+			putchar(',');
+		}
+		else
+		{
+			putchar('$');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
