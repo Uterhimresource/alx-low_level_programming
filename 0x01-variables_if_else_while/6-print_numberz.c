4939bd346f1b5da713540a7942c53c94613265d3
Uterhimresource <uterhimresource@gmail.com>
2023-02-17 16:59:31 -0800

    display base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function where excution begin
+ * Description: display base 10 numbers from 0 t0 9
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
