c00508d03a9ca0c296df0dd386f36ff3659f8d69
Uterhimresource <uterhimresource@gmail.com>
2023-02-21 14:18:45 -0800

    print a to z 10 times in lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - where execution begin
+ * Return: 0 when success
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int ctr;
+
+	for (ctr = 1; ctr < 11; ctr++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
