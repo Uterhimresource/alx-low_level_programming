682afedb34243dc77dc2569fc8b39a7da58ddc14
Uterhimresource <uterhimresource@gmail.com>
2023-02-21 15:02:13 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - function
+ * @c: parameter
+ * Return: 0 when success
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return ((-1)c);
+	}
+	else
+	{
+		return (c);
+	}
+}
