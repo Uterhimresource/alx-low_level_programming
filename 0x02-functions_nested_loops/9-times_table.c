f1650942b7ec3f97907a4647b173a1ba0eb6b915
Uterhimresource <uterhimresource@gmail.com>
2023-02-21 16:04:55 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,30 @@ void times_table(void)
 {
 	int n1;
 	int n2;
+	int res;
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = 0; n2 <= 9; n2++)
 		{
-			_putchar((n1 * n2) + '0');
-			_putchar(',');
-			_putchar(' ');
+			res = n1 * n2;
+			if (n2 != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && n2 != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
 		}
 		_putchar('\n');
 	}
