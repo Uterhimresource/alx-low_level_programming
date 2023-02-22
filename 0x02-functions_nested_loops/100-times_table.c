852d7eef4ce756508548ad52740e7df732035016
Uterhimresource <uterhimresource@gmail.com>
2023-02-21 16:51:14 -0800

    time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_times_table - function
+ * @n: variable
+ * Return: 0 when success
+ */
+void print_times_table(int n)
+{
+	int n1;
+	int n2;
+	int res;
+
+	for (n1 = 0; n1 <= n; n1++)
+	{
+		for (n2 = 0; n2 <= n; n2++)
+		{
+			res = n1 * n2;
+			if (n2 != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && n2 != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
