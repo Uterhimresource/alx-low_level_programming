cf2048ba937c7ee3a72ccfcbb65ddc6ee35105c2
Uterhimresource <uterhimresource@gmail.com>
2023-02-21 16:33:13 -0800

    loop

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - function
+ * @n: upper bound
+ * Return: 0 when success
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", 98);
+	}
+	_putchar('\n');
+}
