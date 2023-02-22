cd9d3b439ab7303220cc3fe06fc772d1fc3fdfad
Uterhimresource <uterhimresource@gmail.com>
2023-02-21 16:11:29 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function
+ * @n1: first integer variable
+ * @n2: second integer variable
+ * Return: 0 when success
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
