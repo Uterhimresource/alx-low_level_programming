a37511e6dafdfd26e15cb2e8350c178e047dcf76
Uterhimresource <uterhimresource@gmail.com>
2023-02-23 18:12:31 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function
+ * @n1: first integer variable
+ * @n2: second integer variable
+ * Return: 0 when success
+ */
+int mul(int n1, int n2)
+{
+	return (n1 * n2);
+}
