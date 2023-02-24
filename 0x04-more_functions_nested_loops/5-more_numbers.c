4d44e57cbe71fcc7738f9014d24c65f1ad6b3be3
Uterhimresource <uterhimresource@gmail.com>
2023-02-24 13:43:17 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
-#include "mainn.h"
+#include "main.h"
 
 /**
  * more_numbers - number 1 through 14
  * Return: no return
  */
-
 void more_numbers(void)
 {
 	int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
+			if (j > 9)
+				_putchar('1');
+			_putchar((j % 10) + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
