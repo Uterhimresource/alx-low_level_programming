8e146244d2cf22c072f71a04f19795c9c7755381
Uterhimresource <uterhimresource@gmail.com>
2023-02-27 18:52:49 -0800

    put caharcter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - function of print string character
+ * @str: second variable of parameter
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
