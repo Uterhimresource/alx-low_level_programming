625cd8f81033841af124a455c709275378e925d6
Uterhimresource <uterhimresource@gmail.com>
2023-02-28 16:57:55 -0800

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,13 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, c = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-	for (i = i + 1; i >= 0; i--)
+	{
+		c++;
+	}
+	for (i = c; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
