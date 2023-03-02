4d340176ac4c27b77d0e2f8a07b78cfb1df65d5e
Uterhimresource <uterhimresource@gmail.com>
2023-03-02 05:29:15 -0800

    more pointer, string and pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - pointer function
+ * @dest: pointer variable for string
+ * @src: pointer variable
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, s = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[s] != '\0')
+	{
+		dest[i] = src[s];
+		s++;
+		i++;
+	}
+	return (dest);
+}
