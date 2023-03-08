2c8adba804d225d85b62852fd947a493e0a126af
uterhimresource <uterhimresource@gmail.com>
2023-03-08 13:43:31 -0800

    reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * _print_rev_recursion - function to print string character in reverse
- * @s: string variable
- * Return - nothing
+ * _print_rev_recursion - function
+ * @s: variable of string
+ * Return: string printed in reverse
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		return;
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
