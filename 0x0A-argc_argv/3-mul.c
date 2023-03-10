80ebc630d44cbd91625eed7da0787e2c1815674b
uterhimresource <uterhimresource@gmail.com>
2023-03-10 05:14:07 -0800

    done all

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: length of string
+ * @argv: character or string t
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int mul = 1;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
+		printf("%i\n", mul);
+	}
+	return (0);
+}
