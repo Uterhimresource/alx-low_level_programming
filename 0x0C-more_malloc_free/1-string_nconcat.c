efe6d126b6a80775766b504332bf191644812aca
uterhimresource <uterhimresource@gmail.com>
2023-03-17 00:06:32 -0700

    concatenate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* string_nconcat - function to concatenate string
+* @s1: string one
+* @s2: string two
+* @n: string to be printed
+* Return: success
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int l1, i, e;
+	char *a;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+	l1 = 0;
+	while (s1[l1])
+		l1++;
+
+	a = malloc(sizeof(*a) * l1 + n + 1);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0, e = 0; i < (l1 + n); i++)
+	{
+		if (i < l1)
+		{
+			a[i] = s1[i];
+		}
+		else
+		{
+			a[i] = s2[e++];
+		}
+	}
+	a[i] = '\0';
+	return (a);
+}
