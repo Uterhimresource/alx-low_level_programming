8a6b7d6a77ab0ff9377f70fa4b515bd3fd9f08cb
uterhimresource <uterhimresource@gmail.com>
2023-03-20 12:51:54 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ * @name: name of dog variable
+ * @age: age of dog variable
+ * @owner: owner of dog variable
+ * Description: about dog data
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
